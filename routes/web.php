57aa3446dfc1cd24e88502a541e217288110a11f
CatherineSuazo0306 <catherine.suazo@unah.hn>
2025-02-18 14:35:09 -0600

    Commit

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
+
+Los modales solo son para hacer avisos no para hacer formularios
+No usar en exceso los modales
+prohiido los alerts de javascript , se pueden usar los de bostrap pero no utilizar los alerts de javascript
+Ya que los botones estan programados por si solos
+Nada de alerts
 */
 
 Route::get('/', function () {
