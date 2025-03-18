938275556f53b37efd2db03815b9c1fd80c4b318
Gisel402 <giselcruz@unah.hn>
2025-03-18 00:01:43 -0600

    modificaciones del login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -51,10 +51,11 @@ class LoginController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => 'user', // Asignando valor por defecto
         ]);
 
         Auth::login($user);
 
-        return redirect('/home');
+        return redirect('/HomeUser');
     }
 }
