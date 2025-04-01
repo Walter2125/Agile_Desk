a253e9fcaa5b9bd0d9bf7f8ad33be178a8c39c2e
Walter2125 <wavalladaresv@unah.hn>
2025-04-01 12:54:45 -0600

    modificacion del controlador de los providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Notificaciones;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Compartir notificaciones con todas las vistas
+    View::composer('*', function ($view) {
+        if (Auth::check()) {
+            $notificaciones = Notificaciones::where('user_id', Auth::id())
+                                          ->where('read', false)
+                                          ->get();
+            $view->with('notificaciones', $notificaciones);
+        } else {
+            $view->with('notificaciones', collect([])); // Para evitar errores si no está autenticado
+        }
+    });
     }
 }
