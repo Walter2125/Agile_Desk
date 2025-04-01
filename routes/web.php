ac6e4c21873279ad158a8493ff63833689299d37
Walter2125 <wavalladaresv@unah.hn>
2025-04-01 12:59:10 -0600

    modificacion de las rutas para añadir las rutas de las notificaciones

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\FormatohistoriaControler;
 use App\Http\Controllers\FullCalendarController;
 use App\Http\Controllers\HistoriaController;
 use App\Http\Controllers\HistorialCambiosController;
-
+use App\Http\Controllers\NotificacionesController;
 use App\Http\Controllers\TableroController;
 
 
@@ -99,3 +99,11 @@ Route::get('/register', [LoginController::class, 'showRegisterForm'])->name('reg
 Route::post('/register', [LoginController::class, 'register']);
 
 Route::post('/actualizar-estado', [HistoriaController::class, 'actualizarEstado'])->name('actualizar.estado');
+
+Route::middleware(['auth'])->group(function () {
+    // Notificaciones routes
+    Route::get('/notificaciones', [NotificacionesController::class, 'index'])->name('notificaciones.index');
+    Route::post('/notificaciones/{id}/read', [NotificacionesController::class, 'markAsRead'])->name('notificaciones.markAsRead');
+    Route::post('/notificaciones/read-all', [NotificacionesController::class, 'markAllAsRead'])->name('notificaciones.markAllAsRead');
+    Route::delete('/notificaciones/{id}', [NotificacionesController::class, 'destroy'])->name('notificaciones.destroy');
+});
